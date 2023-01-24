fad729c42ef3d57797f2479e91500f7f06ffa746
EetuEkman <eetu.ekman@hotmail.com>
2023-01-24 09:29:33 +0200

    Added the station controller.

---
+++ solita-dev-academy-2023-server/Controllers/StationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace solita_dev_academy_2023_server.Controllers
+{
+    public class StationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
