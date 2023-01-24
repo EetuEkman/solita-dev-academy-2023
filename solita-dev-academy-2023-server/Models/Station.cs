f30a6938dff4115f9aaca0d76435d06c654bc399
EetuEkman <eetu.ekman@hotmail.com>
2023-01-24 15:16:50 +0200

    Added the station model.

---
+++ solita-dev-academy-2023-server/Models/Station.cs
@@ -0,0 +1,16 @@
+﻿namespace solita_dev_academy_2023_server.Models
+{
+    public record Station
+    {
+        public string Id { get; init; }
+        public string Name_fi { get; init; }
+        public string Name_se { get; init; }
+        public string Name_en { get; init; }
+        public string Address_fi { get; init; }
+        public string Address_se { get; init; }
+        public string Operator { get; init; }
+        public int Capacity { get; init; }
+        public double X { get; init; }
+        public double Y { get; init; }
+    }
+}
