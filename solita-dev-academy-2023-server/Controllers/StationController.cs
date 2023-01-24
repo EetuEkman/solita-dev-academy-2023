f16c03fcf922003b9721cb815ab372ed5016a971
EetuEkman <eetu.ekman@hotmail.com>
2023-01-24 14:09:42 +0200

    Station controller index action queries database. Added turst server certificate option to the connection string.

--- solita-dev-academy-2023-server/Controllers/StationController.cs
+++ solita-dev-academy-2023-server/Controllers/StationController.cs
@@ -1,12 +1,57 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 
 namespace solita_dev_academy_2023_server.Controllers
 {
+    [ApiController]
+    [Route("api/[controller]")]
+    
     public class StationController : Controller
     {
-        public IActionResult Index()
+        private readonly IConfiguration configuration;
+        public StationController(IConfiguration configuration)
         {
-            return View();
+            this.configuration = configuration;
+        }
+        [HttpGet]
+        [Produces("text/plain")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(500)]
+        public async Task<IActionResult> Index()
+        {
+            var connectionString = configuration.GetValue<string>("ConnectionStrings:Citybikes");
+
+            try
+            {
+                using (var connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    var query = "SELECT *" +
+                        "FROM [dbo].[Stations]";
+
+                    using (var command = new SqlCommand(query, connection))
+                    {
+                        using (var reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                    
+                            }
+                        }
+                    }
+                }
+            }
+
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.ToString());
+
+                return StatusCode(500);
+            }
+            
+
+            return Content("List of bike stations.");
         }
     }
 }
