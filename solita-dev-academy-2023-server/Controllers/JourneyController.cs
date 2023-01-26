eeba5d69fb20d4f98543dc460514f9f6a1d73eb9
EetuEkman <eetu.ekman@hotmail.com>
2023-01-26 16:31:28 +0200

    Added a journey controller.

---
+++ solita-dev-academy-2023-server/Controllers/JourneyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace solita_dev_academy_2023_server.Controllers
+{
+    public class JourneyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
