3e0e63bdcc16d39cabba6819e2e17b8afdd8f176
EetuEkman <eetu.ekman@hotmail.com>
2023-01-26 12:18:51 +0200

    Decorated station model properties with column attributes. Changed station model properties names to pascal case.

--- solita-dev-academy-2023-server/Models/Station.cs
+++ solita-dev-academy-2023-server/Models/Station.cs
@@ -1,16 +1,28 @@
-﻿namespace solita_dev_academy_2023_server.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace solita_dev_academy_2023_server.Models
 {
     public record Station
     {
-        public string Id { get; init; }
-        public string Name_fi { get; init; }
-        public string? Name_se { get; init; }
-        public string? Name_en { get; init; }
-        public string? Address_fi { get; init; }
-        public string? Address_se { get; init; }
+        [Column("Id")] 
+        public string? Id { get; init; }
+        [Column("Name_fi")]
+        public string? NameFi { get; init; }
+        [Column("Name_se")]
+        public string? NameSe { get; init; }
+        [Column("Name_en")]
+        public string? NameEn { get; init; }
+        [Column("Address_fi")]
+        public string? AddressFi { get; init; }
+        [Column("Address_se")]
+        public string? AddressSe { get; init; }
+        [Column("Operator")]
         public string? Operator { get; init; }
+        [Column("Capacity")]
         public int? Capacity { get; init; }
+        [Column("X")]
         public decimal? X { get; init; }
+        [Column("Y")]
         public decimal? Y { get; init; }
     }
 }
