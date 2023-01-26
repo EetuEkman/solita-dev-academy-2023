98cdc276d3bacd9c22287f6a5368613bd67f1d1f
EetuEkman <eetu.ekman@hotmail.com>
2023-01-26 18:49:33 +0200

    Build query and dynamic parameters for dapper.

--- solita-dev-academy-2023-server/Controllers/StationController.cs
+++ solita-dev-academy-2023-server/Controllers/StationController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
 using solita_dev_academy_2023_server.Models;
+using System.Collections.Generic;
 using System.Data;
 using System.Text.Json;
 
@@ -32,50 +33,73 @@ namespace solita_dev_academy_2023_server.Controllers
         {
             this.configuration = configuration;
         }
+
         [HttpGet]
         [Produces("application/json")]
         [ProducesResponseType(200)]
         [ProducesResponseType(500)]
-        public async Task<IActionResult> Index([FromQuery] StationQueryParameters parameters)
+        public async Task<IActionResult> Index([FromQuery] StationQueryParameters queryParameters)
         {
-            var connectionString = configuration.GetValue<string>("ConnectionStrings:Citybikes");
+            // Dictionary for building dynamic parameters.
+
+            var dictionary = new Dictionary<string, object>();
+
+            // Build query.
+
+            var query = "SELECT * " +
+                "FROM Stations " +
+                "WHERE 1=1";
+
+            if (queryParameters.NameFi != null)
+            {
+                dictionary.Add("@Name_fi", "%" + queryParameters.NameFi + "%");
+
+                query += " AND Name_fi LIKE @Name_fi";
+            }
+
+            if (queryParameters.NameSe != null)
+            {
+                dictionary.Add("@Name_se", "%" + queryParameters.NameSe + "%");
+
+                query += " AND Name_se LIKE @Name_se";
+            }
+
+            if (queryParameters.NameEn != null)
+            {
+                dictionary.Add("@Name_en", "%" + queryParameters.NameEn + "%");
+
+                query += " AND Name_en LIKE @Name_en";
+            }
+
+            if (queryParameters.AddressFi != null)
+            {
+                dictionary.Add("@Address_fi", "%" + queryParameters.AddressFi + "%");
+
+                query += " AND Address_fi LIKE @Address_fi";
+            }
+
+            if (queryParameters.AddressSe != null)
+            {
+                dictionary.Add("@Address_se", "%" + queryParameters.AddressSe + "%");
+
+                query += " AND Address_se LIKE @Address_se";
+            }
+
+            // Dapper parameters.
+
+            var parameters = new DynamicParameters(dictionary);
 
             List<Station> stations;
 
-            var query = "SELECT * FROM Stations WHERE 1=1";
-
-            if (parameters.NameFi != null)
-            {
-                query += $" AND Name_fi LIKE '%{parameters.NameFi}%'"; 
-            }
-
-            if (parameters.NameSe != null)
-            {
-                query += $" AND Name_se LIKE '%{parameters.NameSe}%'";
-            }
-
-            if (parameters.NameEn != null)
-            {
-                query += $" AND Name_en LIKE '%{parameters.NameEn}%'"; 
-            }
-
-            if (parameters.AddressFi != null)
-            {
-                query += $" AND Address_fi LIKE '%{parameters.AddressFi}%'"; 
-            }
-
-            if (parameters.AddressSe != null)
-            {
-                query += $" AND Address_se LIKE '%{parameters.AddressSe}%'"; 
-            }
-
             try
             {
+                var connectionString = configuration.GetValue<string>("ConnectionStrings:Citybikes");
+
                 using (var connection = new SqlConnection(connectionString))
                 {
                     connection.Open();
 
-                    stations = connection.Query<Station>(query).ToList();
+                    stations = connection.Query<Station>(query, parameters).ToList();
 
                     // Moved from the pure ADO.NET to Dapper.
 
--- solita-dev-academy-2023-server/Models/Journey.cs
+++ solita-dev-academy-2023-server/Models/Journey.cs
@@ -1,15 +1,15 @@
 ﻿namespace solita_dev_academy_2023_server.Models
 {
-    public record Journey
+    public class Journey
     {
-        public int? Id { get; init; }
-        public DateTime? Departure { get; init; }
-        public DateTime? Return { get; init; }
-        public string? Departure_station_id { get; init; }
-        public string? Departure_station_name { get; init; }
-        public string? Return_station_id { get; init; }
-        public string? Return_station_name { get; init; }
-        public double? Covered_distance { get; init; }
-        public double? Duration { get; init; }
+        public int? Id { get; set; }
+        public DateTime? Departure { get; set; }
+        public DateTime? Return { get; set; }
+        public string? Departure_station_id { get; set; }
+        public string? Departure_station_name { get; set; }
+        public string? Return_station_id { get; set; }
+        public string? Return_station_name { get; set; }
+        public double? Covered_distance { get; set; }
+        public double? Duration { get; set; }
     }
 }
