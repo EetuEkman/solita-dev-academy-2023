3e69016a96187b9583dc31c7fd51ecb43a39206e
EetuEkman <eetu.ekman@hotmail.com>
2023-01-26 16:51:11 +0200

    Added the journey model. Added the journey query parameters. Added decorations to the journey controller.

--- solita-dev-academy-2023-server/Controllers/JourneyController.cs
+++ solita-dev-academy-2023-server/Controllers/JourneyController.cs
@@ -1,12 +1,35 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using solita_dev_academy_2023_server.Models;
+using System.Text.Json;
+
 
 namespace solita_dev_academy_2023_server.Controllers
 {
+    public class JourneyQueryParameters
+    {
+        public string? DateFrom { get; set; }
+        public string? DateTo { get; set; }
+        public int? CoveredDistanceFrom { get; set; }
+        public int? CoveredDistanceTo { get; set; }
+        public int? DurationFrom { get; set; }
+        public int? DurationTo { get; set; }
+        public string? DepartureStationName { get; set; }
+        public string? ReturnStationName { get; set; }
+
+    }
+    [ApiController]
+    [Route("api/[controller]")]
     public class JourneyController : Controller
     {
-        public IActionResult Index()
+        List<Journey> Journeys = new();
+
+        [HttpGet]
+        [Produces("application/json")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(500)]
+        public async Task<IActionResult> Index(JourneyQueryParameters parameters)
         {
-            return View();
+            return Json(Journeys);
         }
     }
 }
---
+++ solita-dev-academy-2023-server/Models/Journey.cs
@@ -0,0 +1,15 @@
+﻿namespace solita_dev_academy_2023_server.Models
+{
+    public record Journey
+    {
+        public int? Id { get; init; }
+        public DateTime? Departure { get; init; }
+        public DateTime? Return { get; init; }
+        public string? Departure_station_id { get; init; }
+        public string? Departure_station_name { get; init; }
+        public string? Return_station_id { get; init; }
+        public string? Return_station_name { get; init; }
+        public double? Covered_distance { get; init; }
+        public double? Duration { get; init; }
+    }
+}
