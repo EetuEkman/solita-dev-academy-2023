8c71d02e2abf5e46aee1eefcd6e5565f4b3f7ad7
EetuEkman <eetu.ekman@hotmail.com>
2023-01-27 17:57:24 +0200

    Added the journey result class representing a part of the result set of the query results and links to the next and previous parts.

---
+++ solita-dev-academy-2023-server/Models/JourneyResult.cs
@@ -0,0 +1,9 @@
+﻿namespace solita_dev_academy_2023_server.Models
+{
+    public class JourneyResult
+    {
+        public List<Journey> Journeys { get; set; }
+        public string? Next { get; set; }
+        public string? Previous { get; set; }
+    }
+}
