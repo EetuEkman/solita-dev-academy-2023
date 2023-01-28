977c07982bc08aba2fcef2b86fed53f42afaed24
EetuEkman <eetu.ekman@hotmail.com>
2023-01-28 17:59:54 +0200

    Added the query template and a dictionary for dynamic parameters to journey controller index action.

--- solita-dev-academy-2023-server/Controllers/JourneyController.cs
+++ solita-dev-academy-2023-server/Controllers/JourneyController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
 using solita_dev_academy_2023_server.Models;
 using System.Text.Json;
 
@@ -15,21 +16,67 @@ namespace solita_dev_academy_2023_server.Controllers
         public int? DurationTo { get; set; }
         public string? DepartureStationName { get; set; }
         public string? ReturnStationName { get; set; }
+        public int? Page { get; set; }
 
     }
+    
     [ApiController]
     [Route("api/[controller]")]
     public class JourneyController : Controller
     {
-        List<Journey> Journeys = new();
-
         [HttpGet]
         [Produces("application/json")]
         [ProducesResponseType(200)]
         [ProducesResponseType(500)]
-        public async Task<IActionResult> Index(JourneyQueryParameters parameters)
+        public async Task<IActionResult> Index([FromQuery]JourneyQueryParameters queryParameters)
         {
-            return Json(Journeys);
+            var dictionary = new Dictionary<string, object>()
+            {
+                { "@Date_from", "%" },
+                { "@Date_to", "%" },
+                { "@CoveredDistanceFrom", "%" },
+                { "@CoveredDistanceTo", "%" },
+                { "@DurationFrom", "%" },
+                { "@DurationTo", "%" },
+                { "@DepartureStationName", "%" },
+                { "@ReturnStationName", "%" }
+           };
+
+            var query = "SELECT J.Departure, J.Return, J.Covered_distance, J.Duration," +
+                " DS.Name_fi, DS.Name_se, DS.Name_en, DS.Address_fi, DS.Address_se," +
+                " RS.Name_fi, RS.Name_se, RS.Name_en, RS.Address_fi, RS.Address_se" +
+                " FROM Journeys AS J" +
+                " INNER JOIN Stations AS DS ON J.Departure_station_id = S.Id" +
+                " INNER JOIN AS RS ON J.Return_station_id = S.Id" +
+                " ORDER BY J.Departure DESC, J.Return DESC" +
+                " OFFSET @Offset";
+
+            // Limit the number of rows returned by the query.
+
+            var offset = 0;
+
+            if (queryParameters.Page is not null)
+            {
+                offset = ((int)queryParameters.Page * 20) + 1;
+
+                query += "FETCH NEXT 20 ROWS ONLY";
+            }
+            else
+            {
+                query += "FETCH FIRST 20 ROWS ONLY";
+            }
+
+            dictionary.Add("@Offset", offset);
+
+            var parameters = new DynamicParameters(dictionary);
+
+            var result = new JourneyResult();
+
+            List<Journey> Journeys = new();
+
+            result.Journeys = Journeys;
+
+            return Json(result);
         }
     }
 }
