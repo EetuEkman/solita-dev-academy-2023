059fbd41df8da0cc820ae82aa096cb8c1bf21b19
EetuEkman <eetu.ekman@hotmail.com>
2023-02-03 15:48:00 +0200

    Added a function to fetch journeys into a journey page model.

--- solita-dev-academy-2023-client/src/App.tsx
+++ solita-dev-academy-2023-client/src/App.tsx
@@ -1,10 +1,15 @@
 import * as React from 'react';
 import { hot } from "react-hot-loader/root";
 
-function App() {
+const BASE_URL = "https://localhost:7263/";
+
+const JOURNEYS_URL = new URL("https://localhost:7263/api/Journeys");
+
+function App() {  
   return (
-    <>
-    </>
+    <div>
+      
+    </div>
   )
 }
 
---
+++ solita-dev-academy-2023-client/src/FetchJourneys.ts
@@ -0,0 +1,11 @@
+import JourneyPage from "./Models/JourneyPage";
+
+export default async function FetchJourneys(url: URL) {
+    const response = await fetch(url);
+
+    const json = await response.json();
+
+    const journeys = JSON.parse(json) as JourneyPage;
+
+    return journeys;
+}
