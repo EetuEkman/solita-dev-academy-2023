962a116acca7d66aec4b74079ba9579f5e7d484f
EetuEkman <eetu.ekman@hotmail.com>
2023-02-03 21:19:33 +0200

    Added journey table component.

---
+++ solita-dev-academy-2023-client/src/Journeys/JourneyTable.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Journey from "../Models/Journey";
+
+interface Props {
+    journeys: Journey[]
+}
+
+export default function JourneyTable(props: Props)
+{
+    return (
+        <div></div>
+    )
+}
