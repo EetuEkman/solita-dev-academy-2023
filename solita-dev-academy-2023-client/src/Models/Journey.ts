06b0702b69a9169e7f0c2719444ba7ffa9071143
EetuEkman <eetu.ekman@hotmail.com>
2023-02-03 13:56:47 +0200

    Added journey and journey page models.

---
+++ solita-dev-academy-2023-client/src/Models/Journey.ts
@@ -0,0 +1,16 @@
+export default interface Journey {
+    Departure: Date;
+    Return: Date;
+    Departure_station_name_fi: string;
+    Departure_station_name_se: string;
+    Departure_station_name_en: string;
+    Departure_station_address_fi: string;
+    Departure_station_address_se: string;
+    Return_station_name_fi: string;
+    Return_station_name_se: string;
+    Return_station_name_en: string;
+    Return_station_address_fi: string;
+    Return_station_address_se: string;
+    Covered_distance: number;
+    Duration: number
+}
---
+++ solita-dev-academy-2023-client/src/Models/JourneyPage.tsx
@@ -0,0 +1,9 @@
+import Journey from "./Journey";
+
+export default interface JourneyPage {
+    Journeys: Journey[];
+    Count: number;
+    Previous: string;
+    CurrentPage: number;
+    Next: string;
+}
