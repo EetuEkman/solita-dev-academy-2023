467f3f8116b211d3ab1de0a1109b94680723a9a8
EetuEkman <eetu.ekman@hotmail.com>
2023-02-03 22:08:13 +0200

    Journey table displays journeys.

--- solita-dev-academy-2023-client/src/App.tsx
+++ solita-dev-academy-2023-client/src/App.tsx
@@ -1,29 +1,44 @@
 import * as React from 'react';
 import { hot } from "react-hot-loader/root";
 import FetchJourneys from './FetchJourneys';
+import JourneyTable from './Journeys/JourneyTable';
 import JourneyPage from './Models/JourneyPage';
-
-const BASE_URL = "https://localhost:7263/";
 
 const JOURNEYS_URL = new URL("https://localhost:7263/api/Journey");
 
 function App() {
   const [page, setPage] = React.useState<JourneyPage>();
 
+  const [isWorking, setIsWorking] = React.useState(false);
+
   const handleFetchClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    fetchJourneys();
+    if (isWorking) {
+      return;
+    }
+
+    fetchJourneys();    
   }
 
   async function fetchJourneys() {
+    setIsWorking(isWorking => true);
+
     let page = await FetchJourneys(JOURNEYS_URL);
 
     setPage(p => page);
+
+    setIsWorking(isWorking => false);
   }
 
   return (
     <div>
       <button onClick={handleFetchClick}>Fetch</button>
-      {JSON.stringify(page, null, 2)}
+      {
+        page ?
+          <JourneyTable journeys={page?.Journeys}></JourneyTable>
+          :
+          null
+      }
+
     </div>
   )
 }
--- solita-dev-academy-2023-client/src/Journeys/JourneyTable.tsx
+++ solita-dev-academy-2023-client/src/Journeys/JourneyTable.tsx
@@ -5,9 +5,53 @@ interface Props {
     journeys: Journey[]
 }
 
-export default function JourneyTable(props: Props)
-{
+export default function JourneyTable(props: Props) {
     return (
-        <div></div>
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Departure</th>
+                        <th>Return</th>
+                        <th>Departure station</th>
+                        <th>Departure station se</th>
+                        <th>Departure station en</th>
+                        <th>Departure station address</th>
+                        <th>Departure station address se</th>
+                        <th>Return station </th>
+                        <th>Return station se</th>
+                        <th>Return station en</th>
+                        <th>Return station address</th>
+                        <th>Return station address se</th>
+                        <th>Covered distance</th>
+                        <th>Duration</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        props.journeys.map((journey, index) => {
+                            return (
+                                <tr key={index}>
+                                    <td>{new Date(journey.Departure).toLocaleString()}</td>
+                                    <td>{new Date(journey.Return).toLocaleString()}</td>
+                                    <td>{journey.Departure_station_name_fi}</td>
+                                    <td>{journey.Departure_station_address_se}</td>
+                                    <td>{journey.Departure_station_name_en}</td>
+                                    <td>{journey.Departure_station_address_fi}</td>
+                                    <td>{journey.Departure_station_address_se}</td>
+                                    <td>{journey.Return_station_name_fi}</td>
+                                    <td>{journey.Return_station_name_se}</td>
+                                    <td>{journey.Return_station_name_en}</td>
+                                    <td>{journey.Return_station_address_fi}</td>
+                                    <td>{journey.Return_station_address_se}</td>
+                                    <td>{journey.Covered_distance}</td>
+                                    <td>{journey.Duration}</td>
+                                </tr>
+                            )
+                        })
+                    }
+                </tbody>
+            </table>
+        </div>
     )
 }
