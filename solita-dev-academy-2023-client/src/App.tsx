6c3089837c9124a1bec1f14d1f990ce342390a38
EetuEkman <eetu.ekman@hotmail.com>
2023-02-13 18:52:11 +0200

    Added navigation bar.

--- solita-dev-academy-2023-client/src/App.tsx
+++ solita-dev-academy-2023-client/src/App.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import { hot } from "react-hot-loader/root";
-import { BrowserRouter as Router,Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link, useRoutes } from 'react-router-dom';
 import JourneysPage from './Pages/JourneysPage';
 import StationsPage from './Pages/StationsPage';
 
 function App() {
   return (
     <Router>
+      <nav className="flex flex-row justify-start items-center gap-4 p-4 text-yellow-500 bg-bluish_grey-500">
+        <div className="inline-block font-bold m-2">Helsinki city bikes</div>
+        <Link to="/journeys">Journeys</Link>
+        <Link to="/stations">Stations</Link>
+      </nav>
       <Routes>
         <Route path="/" element={<JourneysPage></JourneysPage>}></Route>
         <Route path="/journeys" element={<JourneysPage></JourneysPage>}></Route>
         <Route path="/stations" element={<StationsPage></StationsPage>}></Route>
       </Routes>
     </Router>
-    
+
   )
 }
 
