929c1ade97ad805f484dc122ff128a0dfef9beba
EetuEkman <eetu.ekman@hotmail.com>
2023-02-13 18:35:23 +0200

    Added the stations page.

--- solita-dev-academy-2023-client/src/App.tsx
+++ solita-dev-academy-2023-client/src/App.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import { hot } from "react-hot-loader/root";
 import { BrowserRouter as Router,Routes, Route, Link } from 'react-router-dom';
 import JourneysPage from './Pages/JourneysPage';
+import StationsPage from './Pages/StationsPage';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<JourneysPage></JourneysPage>}></Route>
+        <Route path="/journeys" element={<JourneysPage></JourneysPage>}></Route>
+        <Route path="/stations" element={<StationsPage></StationsPage>}></Route>
       </Routes>
     </Router>
     
