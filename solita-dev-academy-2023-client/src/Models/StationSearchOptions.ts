271987ffdb66f1da22efc927bdda3380aff0d6ee
EetuEkman <eetu.ekman@hotmail.com>
2023-02-16 18:08:35 +0200

    Added station search options model.

---
+++ solita-dev-academy-2023-client/src/Models/StationSearchOptions.ts
@@ -0,0 +1,9 @@
+export default interface StationSearchOptions {
+    Name_fi: string;
+    Name_se: string;
+    Name_en: string;
+    Address_fi: string;
+    Address_se: string;
+    Operator: string;
+    Capacity?: number;
+}
