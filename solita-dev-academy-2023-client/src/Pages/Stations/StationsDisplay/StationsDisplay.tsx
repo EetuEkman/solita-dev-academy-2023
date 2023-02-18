5506335068f747e4693ade1ab1baf6eb190089da
EetuEkman <eetu.ekman@hotmail.com>
2023-02-18 14:06:31 +0200

    Added passing of props between stations page and stations display.

--- solita-dev-academy-2023-client/src/Pages/Stations/StationsDisplay/StationsDisplay.tsx
+++ solita-dev-academy-2023-client/src/Pages/Stations/StationsDisplay/StationsDisplay.tsx
@@ -1,7 +1,10 @@
 import React from "react";
+import FetchedStationsPage from "../../../Models/FetchedStationsPage";
 
 interface Props {
-
+    stationsPage: FetchedStationsPage;
+    isWorking: boolean;
+    OnFetchPointerDown: React.PointerEventHandler<HTMLButtonElement>;
 }
 
 export default function StationsDisplay(props: Props) {
--- solita-dev-academy-2023-client/src/Pages/Stations/StationsPage.tsx
+++ solita-dev-academy-2023-client/src/Pages/Stations/StationsPage.tsx
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import DEFAULT_STATION_SEARCH_OPTIONS from "../../Constants/DefaultStationSearchOptions";
+import FetchedStationsPage from "../../Models/FetchedStationsPage";
 import StationSearchOptions from "../../Models/StationSearchOptions";
 import StationsSearch from "./Search/StationsSearch";
+import StationsDisplay from "./StationsDisplay/StationsDisplay";
 
 interface Props {
 
@@ -10,6 +12,10 @@ interface Props {
 export default function StationsPage(props: Props) {
     const [stationSearchOptions, SetStationSearchOptions] = useState<StationSearchOptions>(DEFAULT_STATION_SEARCH_OPTIONS);
 
+    const [stationsPage, SetStationsPage] = useState<FetchedStationsPage | null>();
+
+    const [isWorking, setIsWorking] = useState(false);
+
     function HandleFetchPointerDown(event: React.PointerEvent<HTMLButtonElement>) {
         console.log("Fetch stations not yet implemented.");
     }
@@ -17,6 +23,11 @@ export default function StationsPage(props: Props) {
     return (
         <div className="m-0.5">
             <StationsSearch stationSearchOptions={stationSearchOptions} SetStationSearchOptions={SetStationSearchOptions} OnFetchPointerDown={HandleFetchPointerDown}></StationsSearch>
+            {
+                stationsPage ?
+                    <StationsDisplay stationsPage={stationsPage} OnFetchPointerDown={HandleFetchPointerDown} isWorking={isWorking}></StationsDisplay> :
+                    null
+            }
         </div>
     )
 }
