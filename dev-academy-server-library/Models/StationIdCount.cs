d84207a632ae5ce98299d634c8637d5303e95f96
EetuEkman <eetu.ekman@hotmail.com>
2023-03-19 23:10:57 +0200

    Removed unused projects.

--- dev-academy-server-library/Models/StationIdCount.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace dev_academy_server_library.Models
-{
-    // Used to hold both the top return and departure station ids and the count.
-    public class StationIdCount
-    {
-        public string Id { get; set; }
-        public int Count { get; set; }
-    }
-}
