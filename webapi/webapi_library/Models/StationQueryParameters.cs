dfb10842bedcb4d3c326f62ed444f01ce5cb66fb
EetuEkman <eetu.ekman@hotmail.com>
2023-03-22 15:00:52 +0200

    Added max length model validation attributes to station query parameters.

--- webapi/webapi_library/Models/StationQueryParameters.cs
+++ webapi/webapi_library/Models/StationQueryParameters.cs
@@ -1,12 +1,20 @@
-﻿namespace webapi_library.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace webapi_library.Models
 {
     public class StationQueryParameters
     {
+        [MaxLength(64)]
         public string? NameFi { get; set; }
+        [MaxLength(64)]
         public string? NameSe { get; set; }
+        [MaxLength(64)]
         public string? NameEn { get; set; }
+        [MaxLength(64)]
         public string? AddressFi { get; set; }
+        [MaxLength(64)]
         public string? AddressSe { get; set; }
+        [MaxLength(64)]
         public string? Operator { get; set; }
         public int? CapacityFrom { get; set; }
         public int? CapacityTo { get; set; }
